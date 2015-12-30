01e207f15ec4c99c970a87f0ccfde2520bd71a61
elgs <elgs1980@hotmail.com>
2015-12-30 00:56:46 -0800

    Update.

--- jsonql.go
+++ jsonql.go
@@ -2,24 +2,65 @@
 package jsonql
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/elgs/gosplitargs"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	fmt.Println("Hello World!")
-}
-
 type JSONQL struct {
+	Data interface{}
 }
 
-func NewStringQuery(jsonString string) *JSONQL {
-	return &JSONQL{}
+func NewStringQuery(jsonString string) (*JSONQL, error) {
+	var data = new(interface{})
+	err := json.Unmarshal([]byte(jsonString), data)
+	if err != nil {
+		return nil, err
+	}
+	return &JSONQL{*data}, nil
 }
 
-func NewObjectQuery(jsonObject map[string]interface{}) *JSONQL {
-	return &JSONQL{}
+func NewQuery(jsonObject interface{}) *JSONQL {
+	return &JSONQL{Data: jsonObject}
 }
 
-func NewArrayQuery(jsonArray []interface{}) *JSONQL {
-	return &JSONQL{}
+func (this *JSONQL) Parse(exp string) (interface{}, error) {
+	paths, err := gosplitargs.SplitArgs(exp, "\\.", false)
+	if err != nil {
+		return nil, err
+	}
+	var context interface{} = this.Data
+	for _, path := range paths {
+		if len(path) >= 3 && strings.HasPrefix(path, "[") && strings.HasSuffix(path, "]") {
+			// array
+			index, err := strconv.Atoi(path[1 : len(path)-1])
+			if err != nil {
+				return nil, err
+			}
+			switch v := context.(type) {
+			case []interface{}:
+				{
+					if len(v) <= index {
+						return nil, errors.New("Index out of range.")
+					}
+					context = v[index]
+				}
+			default:
+				return nil, errors.New(fmt.Sprint(path, " is not an array. ", v))
+			}
+
+		} else {
+			// map
+			switch v := context.(type) {
+			case map[string]interface{}:
+				context = v[path]
+			default:
+				return nil, errors.New(fmt.Sprint(path, " is not an object. ", v))
+			}
+		}
+	}
+	return context, nil
 }
---
+++ jsonql_test.go
@@ -0,0 +1,70 @@
+// jsonql_test
+package jsonql
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+
+	jsonString := `
+[
+  {
+    "name": "elgs",
+    "gender": "m",
+    "skills": [
+      "Golang",
+      "Java",
+      "C"
+    ]
+  },
+  {
+    "name": "enny",
+    "gender": "f",
+    "skills": [
+      "IC",
+      "Electric design",
+      "Verification"
+    ]
+  },
+  {
+    "name": "sam",
+    "gender": "m",
+    "skills": [
+      "Eating",
+      "Sleeping",
+      "Crawling"
+    ]
+  }
+]
+`
+	parser, err := NewStringQuery(jsonString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var pass = []struct {
+		in string
+		ex interface{}
+	}{
+		{"[0].name", "elgs"},
+		{"[1].gender", "f"},
+		{"[2].skills.[1]", "Sleeping"},
+	}
+	var fail = []struct {
+		in string
+		ex interface{}
+	}{}
+	for _, v := range pass {
+		result, err := parser.Parse(v.in)
+		if err != nil {
+			t.Error(err)
+		}
+		if v.ex != result {
+			t.Error("Expected:", v.ex, "actual:", result)
+		}
+	}
+	for _, _ = range fail {
+
+	}
+}
